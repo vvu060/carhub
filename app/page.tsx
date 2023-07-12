4622e4dba2af30295bd0c950b5cf8d37c3bcbf8f
vvu060 <vvu060@gmail.com>
2023-07-12 17:24:19 +0530

    use filter props

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import { CarCard, CustomFilter, Hero, SearchBar } from '@/components/';
 import { fetchCars } from '@/utils';
 
-export default async function Home() {
-  const allCars = await fetchCars();
+export default async function Home({ searchParams }) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || '',
+    model: searchParams.model || '',
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || 10,
+  });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   console.log({ allCars });
+
   return (
     <main className='overflow-hidden'>
       <Hero />
