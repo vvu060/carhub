01d0cdc80e7b37ef3a4a03df4f618c2ca3420416
vvu060 <vvu060@gmail.com>
2023-07-12 17:23:48 +0530

    implement car details comp

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import { useState } from 'react';
 
 import { CarProps } from '@/types';
-import { calculateCarRent } from '@/utils';
+import { calculateCarRent, generateCarImageUrl } from '@/utils';
 import CustomButton from './CustomButton';
 import { CarDetails } from './';
 
@@ -33,7 +33,7 @@ const CarCard = ({ car }: CarCardProps) => {
 
       <div className='relative w-full h-40 my-3 object-contain'>
         <Image
-          src='/hero.png'
+          src={generateCarImageUrl(car)}
           alt='car model'
           fill
           priority
